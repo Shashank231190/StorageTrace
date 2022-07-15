c1dcca54958f0279a144fd1f82a7df2fa6e87063
Shashank231190 <33515190+Shashank231190@users.noreply.github.com>
2022-07-15 18:30:33 +0530

    Semi final version of script

---
+++ ScriptXperf_Storage.ps1
@@ -0,0 +1,292 @@
+<#
+This will capture
+ .network dump
+ .memory dump
+ .xperf
+ .Disk.sys trace
+ .iSCSI trace
+ .MPIO/DSM trace
+ .System.evtx
+#>
+
+
+#region LogPath
+$DefaultDrive = "c:\"
+$logDir = "Logs"
+$DefaultLogDir = $DefaultDrive + $logDir
+if (!(test-path -path $DefaultLogDir)) {
+    New-Item -Name $logDir -ItemType Directory -path $DefaultDrive -ErrorAction Stop
+
+}
+
+$xperfToolLocation = "C:\Toolkit"
+
+#endregion
+
+#region Filter
+
+$eventCapTime = -1
+$start = (get-date).AddMinutes($eventCapTime)
+
+$filter153 = @{
+
+    LogName      = "System"
+    ProviderName = "disk"
+    StartTime    = $start 
+    Level        = 3
+    ID           = 153
+  
+}
+#endregion
+
+#region StorageTraces
+[ScriptBlock]$StorageTracesStart = { 
+    
+    "Storport"
+    logman create trace "storport" -ow -o $DefaultLogDir"\storport.etl" -p "Microsoft-Windows-StorPort" 0xffffffffffffffff 0xff -nb 16 16 -bs 1024 -mode Circular -f bincirc -max 1024 -ets
+    "Disk Trace"
+    logman create trace "minkernel_storage_Disk" -ow -o $DefaultLogDir"\minkernel_storage_disk.etl" -p `{945186BF-3DD6-4F3F-9C8E-9EDD3FC9D558`} 0xffffffffffffffff 0xff -nb 16 16 -bs 1024 -mode Circular -f bincirc -max 1024 -ets
+    "iSCSI Trace"
+    logman create trace "drivers_storage_iSCSI" -ow -o $DefaultLogDir"\drivers_storage_iSCSI.etl" -p `{1BABEFB4-59CB-49E5-9698-FD38AC830A91`} 0xffffffffffffffff 0xff -nb 16 16 -bs 1024 -mode Circular -f bincirc -max 1024 -ets
+    "MPIO Trace"
+    logman create trace "drivers_storage_MPIO" -ow -o $DefaultLogDir"\drivers_storage_MPIO.etl" -p `{8E9AC05F-13FD-4507-85CD-B47ADC105FF6`} 0xffffffffffffffff 0xff -nb 16 16 -bs 1024 -mode Circular -f bincirc -max 1024 -ets
+    "DSM Trace"
+    logman create trace "drivers_storage_DSM" -ow -o $DefaultLogDir"\drivers_storage_DSM.etl" -p `{DEDADFF5-F99F-4600-B8C9-2D4D9B806B5B`} 0xffffffffffffffff 0xff -nb 16 16 -bs 1024 -mode Circular -f bincirc -max 1024 -ets
+
+}
+
+
+[ScriptBlock]$StorageTracesStop = {
+
+    logman stop "storport" -ets
+    logman stop "minkernel_storage_Disk" -ets
+    logman stop "drivers_storage_iSCSI" -ets
+    logman stop "drivers_storage_MPIO" -ets
+    logman stop "drivers_storage_DSM" -ets
+
+}
+
+#endregion
+
+#region NetworkTrace
+
+[scriptblock]$NetworkTarceStart = {
+     
+    "Network Trace"
+    $nameHost = hostname
+    $traceName = $nameHost + ".etl"
+    netsh trace start capture=yes maxsize=2048 filemode=circular overwrite=yes report=no tracefile= $DefaultLogDir"\$traceName"
+}
+
+[scriptblock]$NetworkTarceStop =
+{ "Xperf Stop" 
+    Netsh trace stop }
+
+#endregion
+
+#region Xperf
+[ScriptBlock]$XperfStart = {
+
+    "Xperf"
+    $timeStamp = get-date -Format HH_mm_ss
+    $XperfKernelETL = "Start_Xperf" + $timeStamp + "_kernel.etl"
+    $XperfDir = $DefaultLogDir + "\" + $XperfKernelETL
+    Set-Location -Path $xperfToolLocation
+    .\xperf -on PROC_THREAD+LOADER+FLT_IO_INIT+FLT_IO+FLT_FASTIO+FLT_IO_FAILURE+FILENAME+FILE_IO+FILE_IO_INIT+DISK_IO+HARD_FAULTS+DPC+INTERRUPT+CSWITCH+PROFILE+DRIVERS+Latency+DISPATCHER  -stackwalk MiniFilterPreOpInit+MiniFilterPostOpInit+CSwitch+ReadyThread+ThreadCreate+Profile+DiskReadInit+DiskWriteInit+DiskFlushInit+FileCreate+FileCleanup+FileClose+FileRead+FileWrite+FileFlush -BufferSize 4096 -MaxBuffers 4096 -MaxFile 4096 -FileMode Circular -f $XperfDir 
+}
+
+[scriptblock]$XperfStop = {
+    Set-Location -Path $xperfToolLocation
+    .\Xperf -d $DefaultLogDir"\WaitAnalysis.ETL"
+
+}
+#endregion
+
+#region JobStorageNetwork 
+
+$jobStorageNetwork = {
+
+    [ScriptBlock]$StorageTracesStop = {
+
+        logman stop "storport" -ets
+        logman stop "minkernel_storage_Disk" -ets
+        logman stop "drivers_storage_iSCSI" -ets
+        logman stop "drivers_storage_MPIO" -ets
+        logman stop "drivers_storage_DSM" -ets
+    
+    }
+
+    [scriptblock]$NetworkTarceStop = { Netsh trace stop }
+    
+    $eventCapTime = -1
+    $start = (get-date).AddMinutes($eventCapTime)
+
+    $filter153 = @{
+
+        LogName      = "System"
+        ProviderName = "disk"
+        StartTime    = $start 
+        Level        = 3
+        ID           = 153
+      
+    }
+
+    while (1) {
+        $errorMessage = Get-WinEvent -FilterHashtable $filter153 -MaxEvents 1 -ErrorAction SilentlyContinue
+        if ($errorMessage) {
+            .$StorageTracesStop
+            .$NetworkTarceStop
+            Write-Host -ForegroundColor yellow "Storage & network traces collected."
+            break;
+        }
+    }   
+
+}#JobEnd
+
+#endregion
+
+
+
+#region JobXperf
+
+$jobXperf = {
+
+
+    $eventCapTime = -1
+    $start = (get-date).AddMinutes($eventCapTime)
+    $xperfToolLocation = "C:\Toolkit"
+    [scriptblock]$XperfStop = {
+        Set-Location -Path $xperfToolLocation
+        .\Xperf -d c:\Logs\WaitAnalysis.ETL
+    
+    }
+
+    $filter153 = @{
+
+        LogName      = "System"
+        ProviderName = "disk"
+        StartTime    = $start 
+        Level        = 3
+        ID           = 153
+      
+    }
+
+    while (1) {
+        $errorMessage = Get-WinEvent -FilterHashtable $filter153 -MaxEvents 1 -ErrorAction SilentlyContinue
+        if ($errorMessage) {
+            .$XperfStop
+            Write-Host -ForegroundColor yellow "Xperf Collected."
+            break;
+        }
+    }   
+
+}#JobXperfEnd
+
+#endregion
+
+#region iSCSiData
+
+function get-iSCSIData($LogPath,$ToolLocation) {
+
+    try {
+
+        Get-ChildItem $LogPath -ErrorAction Stop
+        Get-ChildItem $ToolLocation -ErrorAction stop
+        Write-Host -ForegroundColor yellow "Starting Traces"
+        .$StorageTracesStart
+        .$NetworkTarceStart
+        .$XperfStart
+        
+       
+        $jobSN = Start-job -ScriptBlock $jobStorageNetwork -name "StorageNetwork"
+        $jobX = start-job  -ScriptBlock $jobXperf -name "Xperf"
+
+        do {
+               
+            receive-job -job $jobSN
+            Write-Host -ForegroundColor Green "To check the job status press 1"
+            Write-Host -ForegroundColor Green "To stop the traces press 2"
+            
+            $command = read-host "Select option"
+            switch ($command) {
+
+                1 { 
+                    
+                    "Job {0} - status {1}" -f $(Get-job -Id $jobSN.Id).Name, (Get-job -id $jobSN.Id).State
+                    "Job {0} - status {1}" -f $(Get-job -Id $jobX.Id).Name, (Get-job -id $jobX.Id).State
+                  
+                    break 
+                }
+                2 { 
+                    if ((Get-Job -id $jobSN.ID).state -eq "Completed" -and (Get-Job -id $jobX.ID).state -eq "Completed") {
+
+                        break;
+                    }
+                    else {
+                        Write-Host -ForegroundColor yellow "Throwing dummy Event 153 to stop the job $($jobSN.Name) & $($JobX.Name)."
+                        Write-Host -ForegroundColor yellow "Current State of the Job"
+                        get-job | FT Id, Name, State
+                        write-eventlog -logname System -source "disk" -EntryType warning -EventID 153 -message "I am dummy!" 
+                        "Waiting for 5 seconds"
+                        start-sleep -s 5
+                        get-job | FT Id, Name, State
+                    }
+
+
+                    break;
+                }
+
+                default { Write-Host -ForegroundColor red "Select the Valid Option" }
+ 
+            }#switch
+
+
+
+        }while ($command -ne 2)
+
+        @{StorageJob = Receive-Job -job $jobSN
+            XperfJob = Receive-Job -job $jobX
+            StoragaJobState = (get-job -id $jobSN.id).State
+            XperfJobState =  (get-job -id $jobX.id).State
+             
+          } | export-clixml -path $DefaultLogDir"\diag.xml"
+
+          
+        receive-job -job  $jobSN
+        receive-job -job $jobX
+        get-job | remove-job -force
+
+         
+        
+
+    }
+    catch {
+
+
+        Write-Host -ForegroundColor yellow "Exception Occured."
+        Write-Host $_.Exception.Message -ForegroundColor Red
+        Write-Host "Line number at which exception occured $($_.InvocationInfo.ScriptLineNumber)" -ForegroundColor Red
+        .$StorageTracesStop
+        .$NetworkTarceStop
+        .$XperfStop
+        get-job | remove-job -force
+
+    }
+
+    finally {
+
+        $logDirName = Get-Date -Format HH_mm_ss
+        New-Item -Name $logDirName -ItemType dir -Path $DefaultLogDir"\"
+        set-location -Path $DefaultLogDir
+        Move-Item -Path $DefaultLogDir"\*.*" -Destination $DefaultLogDir"\"$logDirName 
+    }
+
+  
+
+
+}
+#endregion
+
+get-iSCSIData -LogPath $DefaultLogDir -ToolLocation $xperfToolLocation
+
+        
